410ad2fe6b90bb0503497067072915ba970d52fa
Lucas-Jiang-9723 <102752748+Lucas-Jiang-9723@users.noreply.github.com>
2022-04-04 17:24:39 -0500

    Create Lecture2_Exercise1.py

---
+++ Lecture2_Exercise1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+
+print("It's tough")
+
+print("nice to meet you")
+
+print('thank you!!')
