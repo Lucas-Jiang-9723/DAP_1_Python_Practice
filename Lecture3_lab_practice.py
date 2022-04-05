17e8341a216154d3c8f75b928bb6d17e82a99f7f
Lucas-Jiang-9723 <102752748+Lucas-Jiang-9723@users.noreply.github.com>
2022-04-05 11:44:33 -0500

    Update Lecture3_lab_practice.py
    The problems are solved with comments given.

--- Lecture3_lab_practice.py
+++ Lecture3_lab_practice.py
@@ -1,45 +1,72 @@
 #### fix the following errors!
 
-#1
-x = '10'
+#%%
+#1 x should be an integer
+x = 10
 y = 20
 print(x + y)
 
-#2
+#%%
+#2 The position index starts from 0, hence the indexed length is out of range
 my_list = [40, 50, 60, 70, 80, 100, 200, 400]
-my_list_len = len(my_list)
+my_list_len = len(my_list)-1
 print(my_list[my_list_len])
 
-#3
+#%%
+#3 Missed a pair of () at the end
 my_string = 'hello world'
-print(my_string.upper)
+print(my_string.upper())
 
-#4
+#%%
+#4 Appending should use the append funtion
 z = ['a', 'b', 'c']
-z[3] = 'd'
+z.append('d')
+print(z)
 
+#%%
 #5 why does the x not display 10, followed by the 200?  Fix it so it does.
+#  Use the print function
 x = 10
-x
+print(x)
 y = 20
 print(x * y)
 
-#6
-color = 'My favorite color is {}, what is yours?' % blue
+#%%
+#6 The syntax/format is not correct, should be consistent. blue should be a string
+
+color = 'My favorite color is %s, what is yours?' %'blue'
 print(color)
+
+
+#%%
 
 #### answer the following questions without changing the code given
 
 #7 make the entries in this list unique
+#  converting the list to a set
 schools = ['harris', 'booth', 'crown', 'harris', 'harris']
+schools_unique = set(schools)
+print(schools_unique)
 
+#%%
 #8 change the 'dog' entry to 'cat'
+#  change the tuple to list first and conduct the change
 animals = tuple(['bird', 'horse', 'dog', 'fish'])
+animals_new = list(animals)
+animals_new[2] = 'cat'
+print(animals_new)
 
+#%%
 #9 make this string take a name based on a variable (you can modify the code on this one)
-welcome = 'Hello Sarah, and welcome to Data and Programming!'
+student_name = input("What's your name?")
+welcome = f'Hello {student_name}, and welcome to Data and Programming!'
 print(welcome)
 
+#%%
 #10 separate the words in this string into entries in a list, with only lower-case
 #letters, e.g. ['i', 'love', 'how', ...
+#   use .lower() and .split()
 my_sent = 'I LOVE how spring is super late this year and there are no flowers and it is cold and rainy.'
+
+my_sent_1 = my_sent[:-1].lower().split(" ")
+print(my_sent_1)
