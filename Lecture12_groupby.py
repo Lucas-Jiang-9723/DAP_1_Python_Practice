213a8cea7a320a65536808b2e37276eef6782883
Lucas-Jiang-9723 <102752748+Lucas-Jiang-9723@users.noreply.github.com>
2022-05-09 19:09:04 -0500

    Update Lecture12_groupby.py

--- Lecture12_groupby.py
+++ Lecture12_groupby.py
@@ -40,9 +40,39 @@ diamonds_byclarity.sort_values("clarity")
 print(diamonds_byclarity)
 
 
-#%%
+#%% Group by multiple categories
+diamonds[">1ct"] = diamonds["carat"].map(lambda c: 1 if c > 1 else 0)
+diamonds_byclarity_byct = diamonds.groupby(["clarity", ">1ct"]).mean().round(2)
 
 
+#%% Groupby is itself an object
+test_group = diamonds.groupby("clarity")
+print(test_group.groups) # the groups show that inside the groupby class, the values are stored as dictionaries
 
+# We can look for the data of a specific category/variable using get_group()
+print(test_group.get_group("I1"))
+
+# df.describe() is also a groupby method - shows descriptive info
+print(test_group.describe())
+
+# basic data exploration
+print(test_group.apply(lambda g: g["price"].max()))
+
+#%% Strings in DataFrames
+diamonds_1 = diamonds
+diamonds_1["cut"].str.upper()
+diamonds_1["cut"].str.replace("Ideal", "ideal")
+
+#%% Missing Values df.dropna() keywarg default to value: how = "any"
+diamonds_1.dropna(axis=1, thresh=3) # only keep the columns where 3 or more cells are NaN
+diamonds_1.dropne(how = "all") # drops if all cells are NaN
+
+# we can fill na values using df.fillna()
+diamonds_1.fillna(0)
+diamonds_1.fillna(method = "backfill") # - takes the value below it and fills in
+# specify what value to fill in na depending on the column name
+diamonds_1.fillna({"col1":100, "col2":200})
+# fill one column's missing values from elements from another column: df["col1"].fillna(df["col2"])
+diamonds_1["cut"].fillna(diamonds_1["clarity"])
 #%% group.max() gives out the maximum value of each group
 #   more flexible alternative group.apply(lambda g: g.max())
