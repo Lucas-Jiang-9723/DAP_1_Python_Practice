9512cc17e5edf978589773ce524336f834049e51
Lucas-Jiang-9723 <102752748+Lucas-Jiang-9723@users.noreply.github.com>
2022-07-19 18:41:43 -0500

    Create lecture17_numpy_and_statsmodel.py

---
+++ lecture17_numpy_and_statsmodel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 24 11:12:50 2022
+
+@author: Lucas Jiang
+"""
+
+#%% Library
+import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+#%%
